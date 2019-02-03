f423f4ed9ad4f0188253af0df31aa5b74edc8c95
ParthMehta15 <46846270+ParthMehta15@users.noreply.github.com>
2019-02-03 11:49:43 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pitch",
+    version="0.0.1",
+    author="Parth Mehta",
+    description="To find pitch of a audio file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
