5c78e0de6680775e650df09e1735b9d875fb4669
ParthMehta15 <46846270+ParthMehta15@users.noreply.github.com>
2019-02-04 15:46:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pitch",
-    version="0.0.1",
+    version="0.0.2",
     author="Parth Mehta",
     description="To find pitch of a audio file",
     long_description=long_description,
